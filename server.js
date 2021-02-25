2e52a9dcce2cec80803fef25d21257666a3a61d9
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-02-25 15:50:48 +0900

    denoサーバーのデモファイルを追加した

---
+++ server.js
@@ -0,0 +1,11 @@
+import { Server } from "http://js.sabae.cc/Server.js"
+
+class MyServer extends Server {
+    api(path) {
+        const list = [];
+        list.push("abc")
+        return { name: "jigintern", path: path, list: list[0] };
+    }
+}
+
+new MyServer(8001);
