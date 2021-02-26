4f220270e741f3439ecd1c5cc95efd115a0a33b7
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-02-26 12:08:06 +0900

    再生リストを取得することができた

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,8 +4,33 @@ import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
 
 window.onload = async () => {
     const data = await fetchJSON("api/get-playlist/");
-    alert(data)
     console.log(data);
+
+    $.ajax({
+        type: 'get',
+        url: 'https://www.googleapis.com/youtube/v3/playlistItems', // リクエストURL
+        dataType: 'json',
+        data: {
+            // partは必須で指定が必要とのこと。レスポンスで返してもらいたいデータをカンマ区切りで指定する。snippetがあればとりあえず動画を再生するレスポンスが受け取れる。
+            part: 'snippet',
+             // 再生リストID
+            playlistId: 'PLaHghXmoJ5Vb7CzgxyID4fn5DFmDmNV0m',
+            // デフォルトは5件までしか受け取らないので、取得件数を変更
+            // maxResults: 20, 
+            // API Key
+            key: ''
+        }
+    }).done(function (response) {
+        // 成功
+        response.items.forEach(function (item) {
+            console.log("成功！")
+            var id = item.snippet.resourceId.videoId;
+            console.log(id)
+            $('#list').append('<iframe src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>');
+        });
+    }).fail(function () {
+        // エラー
+    });
 };
 
 let imgNumber = 0;
